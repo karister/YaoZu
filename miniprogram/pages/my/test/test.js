780eb0a0947adf6499ffa144cc7597bab5fe4d69
karister <karister@qq.com>
2021-11-28 16:06:23 +0800

    完成品牌搜索功能

--- miniprogram/pages/my/test/test.js
+++ miniprogram/pages/my/test/test.js
@@ -2,68 +2,80 @@ Page({
   // 页面的初始数据
   data: {
     inputShowed: false,  //初始文本框不显示内容
-    areas: ['1区1栋A001','1区1栋A002']
+    info: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // 1. 获取数据库引用
-    const db = wx.cloud.database()
-    // db.collection('stores').doc('001').get({
-    //   success: function (res) {
+    // // 1. 获取数据库引用
+    // const db = wx.cloud.database()
+    // // 2. 构造查询语句
+    // db.collection('stores').where({
+    //   area: '中心市场'
+    // })
+    // .get({
+    //   success: function(res) {
+    //     // 输出 [{ "title": "The Catcher in the Rye", ... }]
     //     console.log(res.data)
     //     console.log('success')
-    //   },
-    //   fail: function (res) {
-    //     console.error(res.errMsg)
     //   }
     // })
-    // 2. 构造查询语句
-    // collection 方法获取一个集合的引用
-    // where 方法传入一个对象，数据库返回集合中字段等于指定值的 JSON 文档。API 也支持高级的查询条件（比如大于、小于、in 等），具体见文档查看支持列表
-    // get 方法会触发网络请求，往数据库取数据
-    db.collection('stores').where({
-      area: '中心市场'
-    })
-    .get({
-      success: function(res) {
-        // 输出 [{ "title": "The Catcher in the Rye", ... }]
-        console.log(res.data)
-        console.log('success')
-      }
-    })
   },
 
 
 
 
   // 搜索门店数据
-  searchData: function () {
+  searchData: function (e) {
+    var that = this;
+    var inputData = e.detail.value;
+    console.log(inputData);
+    var info_data = [];
+
+    // 1. 获取数据库引用
+    const db = wx.cloud.database()
+    // 2. 构造查询语句
+    db.collection('stores').where({
+      brand: {
+        // 模糊查询 '.*'相当于mysql的like
+        $regex: '.*' + inputData + '.*',
+        // 不区分大小写
+        $options: 'i'
+      }
+    })
+    .get({
+      success: function(res) {
+        console.log('success');
+        console.log(res.data);
+        console.log(res.data.length);
+        for(let i = 0;i < res.data.length; i++) {
+          info_data[i] = res.data[i].brand + '(' + res.data[i].area + ')';
+        }
+        that.setData({
+          info: info_data
+        });
+      }
+    })
     
-    // var inputData = e.detail.value;
-    // var list = this.data.areas;
-    // console.log(inputData);
-    // for(var i = 0; i < list.length; i++) {
-    //   var index = list[i].indexOf(inputData);
-    //   var result = (index === -1) ?false :true;
-    //   if(result) {
-    //     console.log(list[i]);
-    //   }
-    // }
+    
+
+  
   },
   // 使文本框进入可编辑状态
   showInput: function () {
     this.setData({
       inputShowed: true   //设置文本框可以输入内容
     });
+    console.log(this.data.inputShowed);
   },
   // 取消搜索
   hideInput: function () {
     this.setData({
-      inputShowed: false
+      inputShowed: false,
     });
+    console.log(this.data.inputShowed);
   }
 
 
